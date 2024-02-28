6ae6fa5d502a0f29ecafbf5d2171d422ed1770a7
Shoxrux <shoxruxyarashovuz@gmail.com>
2024-02-28 21:40:47 +0500

    Ready

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -3,23 +3,50 @@
 PhoneBookService PhoneBookService = new PhoneBookService();
     while(true)
     {
-        
-    
-
+    PhoneBookService.PrintAllPhoneBooks();
+    Console.WriteLine("-----------------------------------------------");
+    ControlMenu();
+    int input = Input();
+    if (input==0)
+    {
+        break;
     }
+    switch(input)
+    {
+        case 1:
+            PhoneBookService.CreatePhoneBook();
+            break;
+        case 2:
+            PhoneBookService.DeletePhoneBookById();
+            break;
+        case 3:
+            PhoneBookService.PrintPhoneBookById();
+            break;
+        case 4:
+            PhoneBookService.UpdatePhoneBookBy();
+            break;
+        default:
+            Console.WriteLine("Nomalum raqam");
+            break;
+    }
+    Console.Clear();
+
+
+}
 
 void ControlMenu() 
 {
-    Console.WriteLine(" ListContacts");
+
     Console.WriteLine("1) Create Contact");
     Console.WriteLine("2) Delete Contact by Id");
     Console.WriteLine("3) Get Contact by Id");
     Console.WriteLine("4) Update Contact by Id");
+    Console.WriteLine("0) Exit");
  
 }
-int Input(string text)
+int Input()
 {
-    Console.Write(text);
+    Console.Write(" ");
     string userInput = Console.ReadLine();
     return Convert.ToInt32(userInput);
 }
